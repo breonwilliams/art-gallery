ba7ac40399894ac0daeabf74a164a44199cb9b32
breonwilliams <bjwilli83@gmail.com>
2023-09-15 12:54:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Project Name
+
+Brief description of the project, its goals, and any other high-level information.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed on your local machine:
+
+- [Node.js & npm](https://nodejs.org/en/download/)
+- [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) (if you're planning to clone the repository)
+
+## Getting Started
+
+Follow these steps to set up and run this project locally:
+
+### 1. Clone the Repository
+
+To clone the repository to your local machine, run:
+
+git clone https://github.com/your-username/your-repo-name.git
+
+Then, navigate to the project directory:
+
+cd your-repo-name
+
+### 2. Install Dependencies
+
+To install the required npm packages, run:
+
+npm install
+
+### 3. Start the Local Development Server
+
+To start the local development server, run:
+
+npm run start:dev
+
+### 4. Build the project
+
+npm run build
+
+The project should automatically open in your default web browser. If it doesn't, you can access it using the link provided in the terminal, typically http://localhost:9000.
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'assets/images/',
+                            name: '[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'docs'),
+        },
+        compress: true,
+        port: 9000,
+    },    
+};
