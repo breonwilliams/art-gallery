783913b7f022532b03132268b5faeb7c56fcb370
breonwilliams <bjwilli83@gmail.com>
2023-09-24 16:32:52 -0400

    Removed node_modules

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,1 +1,18 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// Add all solid icons to the library
+library.add(fas);
+
+// Replace any existing `<i>` tags with Font Awesome SVGs
+dom.watch();
+
+import { far } from '@fortawesome/free-regular-svg-icons'; // Regular style icons
+import { fab } from '@fortawesome/free-brands-svg-icons'; // Brand icons
+
+library.add(far, fab);
+
+
+
+
 import './photoswipe/init';
