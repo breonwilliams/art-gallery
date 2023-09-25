2a65c64f0bc9160754e67dd5b33d22615b17f040
breonwilliams <bjwilli83@gmail.com>
2023-09-25 18:50:35 -0400

    modify photoswipe script

--- src/js/photoswipe/init.js
+++ src/js/photoswipe/init.js
@@ -7,3 +7,30 @@ const lightbox = new PhotoSwipeLightbox({
   pswpModule: () => import('photoswipe')
 });
 lightbox.init();
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all images within an anchor tag on the page
+  const images = document.querySelectorAll('a > img');
+
+  images.forEach(img => {
+      // Make sure image is fully loaded before getting dimensions
+      if (img.complete) {
+          addDimensionsToParentLink(img);
+      } else {
+          img.onload = () => {
+              addDimensionsToParentLink(img);
+          };
+      }
+  });
+});
+
+function addDimensionsToParentLink(img) {
+  const width = img.naturalWidth;
+  const height = img.naturalHeight;
+  const parentLink = img.parentElement;
+
+  if (parentLink && parentLink.tagName === 'A') {
+      parentLink.setAttribute('data-pswp-width', width);
+      parentLink.setAttribute('data-pswp-height', height);
+  }
+}
